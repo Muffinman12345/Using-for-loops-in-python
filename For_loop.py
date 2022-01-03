c19d678fe3feb30f2779b327edea9de70f681f2a
Nicholas W <77017235+Muffinman12345@users.noreply.github.com>
2022-01-03 17:07:05 -0500

    Create For_loop.py

---
+++ For_loop.py
@@ -0,0 +1,9 @@
+#You don't need to import anything for a for loop
+
+for i in range(19):
+  
+  #The code above is the correct syntax for making a python for loop
+   #You can write whatever you want the program to do in the indented spaces under the loop.
+  print("hello!")
+ 
+#If I run this piece of code, it will print "hello!" 19 times.
